114036709239143abf07bdbab61f549bd739baa3
a.k.a.a.h.l <a.k.afiq@live.com>
2021-10-10 21:08:20 +0800

    Styled up the CartModal component

--- src/components/Navbar/CartModal.js
+++ src/components/Navbar/CartModal.js
@@ -8,6 +8,10 @@ const CartModal = ({ setModal }) => {
   const itemsInCart = useSelector((state) => state.allData.cart).filter(
     (item) => item.quantity > 0
   );
+  const totalPrice = itemsInCart.reduce(
+    (acc, item) => (acc += item.quantity * item.price),
+    0
+  );
   console.log(itemsInCart);
 
   const handleModal = (e) => {
@@ -28,23 +32,25 @@ const CartModal = ({ setModal }) => {
             <div className="mid-left">
               <img src={item.image} alt={item.name} />
               <div className="mid-left-details">
-                <span>{item.displayName}</span>
-                <span>{`$${item.price.toLocaleString()}`}</span>
+                <span className="display-name">{item.displayName}</span>
+                <span className="price">{`$${item.price.toLocaleString()}`}</span>
               </div>
             </div>
 
-            <div className="right-side">
+            <div className="mid-right">
               <button>-</button>
               <p>{item.quantity}</p>
               <button>+</button>
             </div>
-
-            <div className="bottom">
-              <p>Total</p>
-              <p>{`$${(item.quantity * item.price).toLocaleString()}`}</p>
-            </div>
           </div>
         ))}
+
+        <div className="bottom">
+          <p className="total-text">Total</p>
+          <p className="total-display">{`$${totalPrice.toLocaleString()}`}</p>
+        </div>
+
+        <button className="checkout-btn">Checkout</button>
       </div>
     </StyledModal>
   );
@@ -61,7 +67,122 @@ const StyledModal = styled.aside`
   background-color: rgba(0, 0, 0, 0.3);
 
   .modal-container {
+    position: fixed;
+    right: 200px;
+    top: 50px;
+    width: 400px;
     background-color: #ffffff;
-    padding: 20px 15px;
+    padding: 20px 25px;
+    display: flex;
+    flex-direction: column;
+    border-radius: 8px;
+
+    .top {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      h3 {
+        color: #191919;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+      }
+
+      button {
+        color: #888888;
+        opacity: 0.7;
+        font-size: 16px;
+        text-decoration: underline;
+      }
+    }
+
+    .mid {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin-top: 20px;
+
+      .mid-left {
+        align-self: center;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        img {
+          height: 60px;
+          width: 60px;
+          border-radius: 5px;
+          object-fit: cover;
+        }
+
+        .mid-left-details {
+          margin-left: 15px;
+          display: flex;
+          flex-direction: column;
+
+          .display-name {
+            font-weight: 700;
+            font-size: 14px;
+          }
+
+          .price {
+            margin-top: 3px;
+            font-weight: 600;
+            font-size: 14px;
+            color: #888888;
+            opacity: 0.9;
+          }
+        }
+      }
+
+      .mid-right {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #f1f1f1;
+        width: 100px;
+
+        button,
+        p {
+          flex: calc(100 / 3);
+          color: #000000;
+          padding: 8px 12px;
+          font-size: 14px;
+        }
+
+        p {
+          font-weight: 700;
+        }
+      }
+    }
+
+    .bottom {
+      margin-top: 20px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      .total-text {
+        color: #191919;
+        font-weight: 100;
+        text-transform: uppercase;
+      }
+
+      .total-display {
+        color: #191919;
+        font-weight: 800;
+      }
+    }
+
+    .checkout-btn {
+      margin-top: 30px;
+      border: none;
+      background-color: #d87d4a;
+      padding: 12px 20px;
+      font-size: 16px;
+      font-weight: 600;
+      color: #ffffff;
+      text-transform: uppercase;
+    }
   }
 `;
